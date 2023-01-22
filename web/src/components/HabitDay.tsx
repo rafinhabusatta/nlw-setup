20094f6abf8bbb0509bb84e60c05a0c1891d9e13
rafinhabusatta <lacerdabusatta@gmail.com>
2023-01-22 17:02:50 -0300

    criação do component progressBar; criação de interface para SummaryTable e HabitDay; ciração do component NewHabitForm

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,9 +1,34 @@
-// interface HabitProps {
-//   completed: number;
-// }
+import * as Popover from "@radix-ui/react-popover";
+import { ProgressBar } from "./ProgessBar";
+import clsx from "clsx";
 
-export function HabitDay() {
+interface HabitProps {
+  amount: number,
+  completed: number;
+}
+
+export function HabitDay(props: HabitProps) {
+  const completedPercentage = Math.round(props.completed / props.amount * 100);
   return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+    <Popover.Root>
+      <Popover.Trigger className={clsx("w-10 h-10  rounded-lg border-2", {
+        'bg-zinc-900 border-zinc-800' : completedPercentage === 0,
+        'bg-violet-900 border-violet-700' : completedPercentage > 0 && completedPercentage < 20,
+        'bg-violet-800 border-violet-600' : completedPercentage >= 20 && completedPercentage < 40,
+        'bg-violet-700 border-violet-500' : completedPercentage >= 40 && completedPercentage < 60,
+        'bg-violet-600 border-violet-500' : completedPercentage > 0 && completedPercentage < 80,
+        'bg-violet-500 border-violet-400' : completedPercentage >= 80,
+      })}/>
+
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+          <span className="font-semibold text-zinc-400">terça-feira</span>
+          <span className="mt-1 font-extrabold leading-tight text-3xl">17/1</span>
+          <ProgressBar progress={15} />
+        <Popover.Arrow width={16} height={8} className="fill-zic-900" />
+
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   )
 }
--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -12,7 +12,7 @@ const amountOfDaysToFill = minimumSummaryDatesSize - SummaryDates.length
 export function SummaryTable() {
   return (
     <div className="w-full flex">
-      <div className="grid-rows-7 grid-flow-row gap-3">
+      <div className="grid grid-rows-7 grid-flow-row gap-3">
         {WeekDays.map((weekDay,i) => {
           return (
             <div key={`${weekDay}-${i}`} className="text-zind-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
@@ -24,7 +24,11 @@ export function SummaryTable() {
       <div className="grid grid-rows-7 grid-flow-col gap-3">
        {SummaryDates.map((date) => {
         return (
-          <HabitDay key={date.toString()} />
+          <HabitDay 
+            key={date.toString()} 
+            amount={5}
+            completed={Math.round(Math.random() * 5)}
+          />
         )
        })}
        {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((_, i) => {
